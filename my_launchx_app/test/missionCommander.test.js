d8f5171bd5c0aba6b9a14b480e94c8c17c7e58f5
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-04-27 22:00:08 -0500

    Update proyecto

--- my_launchx_app/test/missionCommander.test.js
+++ my_launchx_app/test/missionCommander.test.js
@@ -3,6 +3,5 @@ describe("Unit Tests for Mission Commander Class", () => {
   test('1) Create a mission commander objet', () => {
     const myMissionCommander = new MissionCommander("Woopa")
     expect(myMissionCommander.name).toBe("Woopa");
-    });
-  })
-  
+  });
+})
